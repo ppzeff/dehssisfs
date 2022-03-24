3bbdf7a2cd554245456947f3213b0fff7e509a11
ppzeff <ppzeff@gmail.com>
2022-03-24 21:32:41 +0300

    24032022 учтена зона временная

--- src/main/java/com/ppzeff/dehssisfs/DehssisfsApplication.java
+++ src/main/java/com/ppzeff/dehssisfs/DehssisfsApplication.java
@@ -17,42 +17,47 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties({
-		FileStorageProperties.class
+        FileStorageProperties.class
 })
 public class DehssisfsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DehssisfsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DehssisfsApplication.class, args);
+    }
 
-	@Bean
-	public TaskScheduler taskScheduler() {
-		final ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-		scheduler.setPoolSize(10);
-		return scheduler;
-	}
+    @Bean
+    public TaskScheduler taskScheduler() {
+        System.out.println(new Date());
+        System.out.println("!!!!!!!!!!!!");
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+        System.out.println(new Date());
+        final ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        return scheduler;
+    }
 
-	@Autowired
-	ModelRepo modelRepo;
+    @Autowired
+    ModelRepo modelRepo;
 
-	@Autowired
-	MainService mainService;
+    @Autowired
+    MainService mainService;
 
-	@Scheduled(fixedDelay = 10 * 1000L, initialDelay = 0)
-	public void scheduled1() throws IOException {
-		List<Integer> listAllCode = modelRepo.findAllCode(); // new ArrayList<>();
+    @Scheduled(fixedDelay = 10 * 1000L, initialDelay = 0)
+    public void scheduled1() throws IOException {
+        List<Integer> listAllCode = modelRepo.findAllCode(); // new ArrayList<>();
 
-		List<Model> modelList = mainService.getRateByCode(listAllCode);
+        List<Model> modelList = mainService.getRateByCode(listAllCode);
 
-		for (Model elModel : modelList) {
-			modelRepo.save(elModel);
-			System.out.println(
-					new Date(elModel.getLastUpdate())+ " SAVE " +
-							elModel.toString());
-		}
-	}
+        for (Model elModel : modelList) {
+            modelRepo.save(elModel);
+            System.out.println(
+                    new Date(elModel.getLastUpdate()) + " SAVE " +
+                            elModel.toString());
+        }
+    }
 }
