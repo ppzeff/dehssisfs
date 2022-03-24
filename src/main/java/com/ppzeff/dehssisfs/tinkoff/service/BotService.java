7b0da74a3218b06e226d8d8fea6f6c46f2df40c3
ppzeff <ppzeff@gmail.com>
2022-03-24 22:12:49 +0300

    24032022 график за поледний месяц

--- src/main/java/com/ppzeff/dehssisfs/tinkoff/service/BotService.java
+++ src/main/java/com/ppzeff/dehssisfs/tinkoff/service/BotService.java
@@ -85,6 +85,7 @@ public class BotService extends TelegramLongPollingBot {
             messageBuilder.parseMode(ParseMode.HTML);
             messageText = update.getMessage().getText();
 
+//          логирование действия пользователя
             UserActions userActions = new UserActions();
             userActions.setUserId(telegramUser.getUserId());
             userActions.setMessage(messageText);
@@ -107,6 +108,25 @@ public class BotService extends TelegramLongPollingBot {
                 System.out.println(e.toString());
             }
         }
+
+
+        if (messageText.contains("/month840")) {
+
+            long start = System.currentTimeMillis();
+            String fileName = makeGraphService.makeGraph(840,ONE_DAY*30)+".jpg";
+            SendPhoto photo = new SendPhoto();
+            photo.setChatId(chatId);
+            photo.setPhoto(new InputFile(new File(fileName)));
+            photo.setCaption("График сформирован за: " +  (System.currentTimeMillis()-start) + "милисекунд");
+            try {
+                execute(photo);
+                new File(fileName).delete();
+//                execute(messageBuilder.build());
+            } catch (TelegramApiException e) {
+                System.out.println(e.toString());
+            }
+        }
+
 
         if (messageText.contains("/week840")) {
 
